3bec101a75978611cfa0843f9773bd919dff757f
Davi <160233282+daviiq@users.noreply.github.com>
2025-06-12 22:59:13 +0000

    Atividade 5 concluída

---
+++ Atividade5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Atividade5 {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite outra coisa: ");
+    String palavra2 = scanner.nextLine(); // Lê até o final da linha
+    System.out.println("palavra2: " + palavra2);
+
+    System.out.println("Digite algo: ");
+    String palavra = scanner.next();
+    System.out.println("palavra: " + palavra);  // Lê só até o primeiro espaço
+
+  }
+} 
+
+//Neste caso, descobrimos também que o método next() ao ser citado primeiro
+// não permite a leitura do NextLine() que vem depois, pois o scanner já
+// leu o espaço em branco que vem depois da palavra.
+
