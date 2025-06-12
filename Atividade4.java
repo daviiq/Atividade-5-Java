45afc39614842b186eb6bd2cc658c6dd1ae4b161
Davi <160233282+daviiq@users.noreply.github.com>
2025-06-12 22:43:56 +0000

    Atividade 4 concluída

---
+++ Atividade4.java
@@ -0,0 +1,18 @@
+javaimport java.util.scanner; // O erro 1 estava na importação do Scanner, que deve ser com 'S' maiúsculo
+//import java.util.Scanner;
+
+public class Atividade4 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        // O erro 2 estava na declaração da variável, que deve ser do tipo int
+        // deve ser utilizado para números inteiros, da seguinte forma:
+        // int numero;
+        
+        System.out.print("Digite um número: ");
+        int numero = entrada.nextDouble();
+// O erro 3 está aqui, pois está utilizando double para ler um número inteiro 
+//com nextDouble(),que é para números decimais
+        
+        System.out.println("Número digitado: " + numero);
+    }
+}
