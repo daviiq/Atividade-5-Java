cc221124893f4e8f1fb8b12ca812d9a72c79e7da
Marcos Júnior Lemes ferreira <marcos.jlf@aluno.ifsc.edu.br>
2025-06-12 21:10:18 -0300

    Update Atividade10.java

--- Atividade10.java
+++ Atividade10.java
@@ -10,24 +10,24 @@ public class Atividade10{
         String produto = input.nextLine();
 
 
-        System.out.print("\nDigite o código do produto: ");
+        System.out.print("Digite o código do produto: ");
         int codigo = input.nextInt();
 
 
-        System.out.print("\nDigite o preço unitário: ");
+        System.out.print("Digite o preço unitário: ");
         double unitario = input.nextDouble();
 
 
-        System.out.print("\nDigite a quantidade em estoque: ");
+        System.out.print("Digite a quantidade em estoque: ");
         int quantidade = input.nextInt();
 
-        System.out.print("\nO produto está ativo (true/false): ");
+        System.out.print("O produto está ativo (true/false): ");
         boolean ativo = input.nextBoolean();
 
         double total = unitario * quantidade;
 
-        System.out.printf("Informações do produto: %s" );
+        System.out.printf("\n -------- informações do produto -----\n \nInformações do produto: %s\nCódigo do produto: %d\nPreço unitário do produto: %.2f\nQuantidade em estoque: %d\nProduto está ativo: %b\nValor total em estoque: %.2f%n", produto, codigo, unitario, quantidade, ativo, total);
 
-
+        input.close();
     }
 }
